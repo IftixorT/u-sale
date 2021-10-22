4dba1f71cc33b699327bd4cde3d01d01a696b361
arslonbekXX <arslonbek.lion067@gmail.com>
2021-10-22 22:34:53 +0500

    add new codes

--- src/App.tsx
+++ src/App.tsx
@@ -10,3 +10,16 @@ function App() {
 }
 
 export default App;
+
+
+
+
+/**
+ *  #1 -> git add .
+ *  #2 -> git commit -m "add app.tsx file otabek text"
+ *  #3 -> git pull origin main
+ *  #4 -> git push origin (my-branch)
+ *
+ *
+ * git pull = git fetch + git merge
+ */
